641120fd5cbe8c55c9fc300f245a26c9bc60c0a3
Navinraaj <117885210+Navinraaj@users.noreply.github.com>
2023-02-07 10:22:30 +1100

    V5.0 Navin

---
+++ public/js/UpdateFood.js
@@ -0,0 +1,27 @@
+const homelessPeople = [
+    {
+        rest_id: "1",
+        rname: "Josh",
+        rnumber: "788456345",
+        food_type: "Vegitatian",
+        qty: "1",
+        location: "Burwood east",
+    },
+    {
+      rest_id: "2",
+      rname: "Asan",
+      rnumber: "788456345",
+      food_type: "nonVegitatian",
+      qty: "2",
+      location: "Deakin University",
+    },
+    {
+        rest_id: "5",
+        rname: "Melissa",
+        rnumber: "788456345",
+        food_type: "Vegitatian",
+        qty: "5",
+        location: "University of Melbourne",
+    },
+  ];
+  
--- routes/LFHRoutes.js
+++ routes/LFHRoutes.js
@@ -2,6 +2,9 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const cors = require("cors");
+const mongoose = require("mongoose");
+const bodyPraser = require("bodyPraser");
+
 
 app.use(express.static("public"));
 app.use(express.json());
